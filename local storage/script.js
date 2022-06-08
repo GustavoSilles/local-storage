bfdbf78efc5580ae1160b39f53a56f7bbb40db4d
GustavoSilles <92472220+GustavoSilles@users.noreply.github.com>
2022-06-08 15:08:37 -0300

    Add files via upload

---
+++ local storage/script.js
@@ -0,0 +1,27 @@
+let tarefas = []
+
+const list = function() {
+    
+    let paragrafoTarefas = document.getElementById('tarefas')
+    paragrafoTarefas.innerHTML = ""
+    if (localStorage.getItem('tarefas') != null)
+        tarefas = JSON.parse(localStorage.getItem('tarefas'))
+
+    tarefas.forEach(tarefa => {
+        paragrafoTarefas.innerHTML += tarefa[0] + " - " + tarefa[1] + "<br>"
+
+    });
+}
+
+const add = function () {
+    let data = document.getElementById('data')
+    let tarefa = document.getElementById('tarefa')
+    if (localStorage.getItem('tarefas') != null)
+        tarefas = JSON.parse(localStorage.getItem('tarefas'))
+
+    tarefas.push([data.value, tarefa.value])
+    console.log(tarefas)
+    localStorage.setItem('tarefas', JSON.stringify(tarefas))
+    data.value = ""
+    tarefa.value = ""
+}
